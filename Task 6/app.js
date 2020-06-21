5440fbcffb5ebe8bb1b651ad6124b98d643fd4ed
ifaiq19 <faiq.019@gmail.com>
2020-06-21 17:31:30 +0500

    Task 6

---
+++ Task 6/app.js
@@ -0,0 +1,16 @@
+//            TASK : 06
+
+//            CHAPTER # 21 - 25 ( STRING METHODS )
+//                   EX # 01
+
+var firstName = prompt('Enter your first name: ');
+var lastName = prompt('Enter our last name: ');
+var fullName = firstName + ' ' + lastName;
+alert('Welcome To This Webpage Dear ' + fullName);
+
+//                   EX # 02
+
+var favoritePhone = prompt('enter your favourite phone model : ');
+document.write('My favourite phone model is : ' + favoritePhone + '<br>');
+document.write('Length of String is : '  + (favoritePhone.length) +  '<br>' + '<br>');
+
